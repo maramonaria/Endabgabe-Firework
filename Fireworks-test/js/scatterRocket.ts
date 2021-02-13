9f24d890a4259031eda9cb72ef718677791ebe0b
Ramona <ramona.maria.gihr@hs-furtwangen.de>
2021-02-13 12:59:44 +0100

    ""

--- Fireworks-test/js/scatterRocket.ts
+++ Fireworks-test/js/scatterRocket.ts
@@ -35,7 +35,7 @@ namespace Fireworks {
             let position: Vector;
             for (let i: number = 0; i < this.particleCount; i++) {
                 position = this.explosionCenter.copy();
-                position.add(Vector.getRandom(1, this.dimension * _canvasWidth / 4));
+                position.add(Vector.getRandom(1, this.dimension * (_canvasWidth / 4)));
                 let particle: Particle = new Particle(position, this.particleShape, radiusParticle, this.color);
                 particle.explode();
                 particle.draw(_context);
